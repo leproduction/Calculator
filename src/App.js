eff50bc936da04b8a880c10f1659ce0232939e5b
“leProduction” <haule.jan.20@gmail.com>
2023-12-20 13:28:26 -0500

    4rd commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   const [getInput, setGetInput] = useState('');
